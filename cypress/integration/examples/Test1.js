f4a71fb955b1dbb3d3b5a1506ea5206f2b3a8922
IgorFerro <igor.ferro.parreira@gmail.com>
2019-11-02 23:39:31 -0300

    first commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+describe('My First Test', function() {
+    it('My FirstTest case', function() {
+
+        //cy.visit("https://qa-web.tilix.com.br");
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+     // expect(true).to.equal(true)
+    })
+  })
