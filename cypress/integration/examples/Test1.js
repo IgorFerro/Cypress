5e163d7858a92c1ad710449fd0b192249642a101
Igor Ferro Parreira <ifparreira@uolinc.com>
2019-11-05 14:26:54 -0300

    forth commit

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -13,11 +13,13 @@ describe('My First Test', function() {
         cy.get('.product').should('have.length',5);
         cy.get('.product:visible').should('have.length',4);
         //Parent child chaining
-        cy.get('.products').find('.product').should('have.length',4);
+        cy.get('.products').as('productLocator');
+
+        cy.get('@productLocator').find('.product').should('have.length',4);
         cy.get(':nth-child(3) > .product-image > img');
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click();
           
-cy.get('.products').find('.product').each(($el,index,$list) => {
+cy.get('@productLocator').find('.product').each(($el,index,$list) => {
 
   const textVeg=$el.find('h4.product-name').text()
   if(textVeg.includes('Cashews'))
@@ -25,5 +27,14 @@ cy.get('.products').find('.product').each(($el,index,$list) => {
     $el.find('button').click()
   }
 })
+//assert if the logo text is correctly displayed
+cy.get('.brand').should('have.text','GREENKART')
+
+//this is to print in logs
+cy.get('.brand').then(function(logoelement){
+  cy.log(logoelement.text())
+
+})
+
 })
 })
