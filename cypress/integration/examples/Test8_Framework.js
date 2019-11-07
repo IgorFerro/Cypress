3af7bd7c0526614749e5bc89e07fe0b1a11c4444
Igor Ferro Parreira <ifparreira@uolinc.com>
2019-11-07 07:44:44 -0300

    Validate attribute properties

--- cypress/integration/examples/Test8_Framework.js
+++ cypress/integration/examples/Test8_Framework.js
@@ -6,20 +6,24 @@ describe('My Test', function() {
     
     before(function() {
         // runs once before all tests in the block
+        cy.fixture('example').then(function(data){
+            this.data =data
+        })
       })
     
-
-
-
 
     it('My Test', function() {
     
         //cy.visit("https://qa-web.tilix.com.br");
         cy.visit("https://rahulshettyacademy.com/angularpractice/");
 
-        cy.get('input[name="name"]:nth-child(2)').type("Bob")
-        cy.get('select').select("Male")
-
+        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        cy.get('select').select(this.data.gender)
+     
+      //Validate attribute properties
+      cy.get(':nth-child(4) > .ng-untouched').should('have.value',this.data.name);
+      cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2')
+      cy.get('#inlineRadio3').should('be.disabled')
        
   
 
